ebd98975c8ff712c610d97dedea489e6e61c40e4
kajouz <kajouz@gmail.com>
2025-12-12 10:48:53 +0200

    db connection and cloudinary setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.DB_URL);
+    console.log(`Conencted to MongoDB: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
