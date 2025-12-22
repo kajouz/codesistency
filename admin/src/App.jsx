7e1c428e48cc3b0231bb430cc09845cb79d4c010
kajouz <kajouz@gmail.com>
2025-12-22 21:36:20 +0200

    add dashboard setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage.jsx";
+import { useAuth } from "@clerk/clerk-react";
+import DashboardPage from "./pages/DashboardPage";
+import ProductsPage from "./pages/ProductsPage";
+import OrdersPage from "./pages/OrdersPage";
+import CustomersPage from "./pages/CustomersPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+
+import PageLoader from "./components/PageLoader";
 
 function App() {
-  return (
-    <div>
-      <h1>Home Page</h1>
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) return <PageLoader />;
 
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+  return (
+    <Routes>
+      <Route path="/login" element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />} />
+
+      <Route path="/" element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}>
+        <Route index element={<Navigate to={"dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+      </Route>
+    </Routes>
   );
 }
 
