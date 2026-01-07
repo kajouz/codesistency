8ba0c01550423f49335829aaa2016f55c7def829
kajouz <kajouz@gmail.com>
2026-01-07 15:19:38 +0200

    Finalizing the admin portal

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,13 +1,10 @@
 import { SignIn } from "@clerk/clerk-react";
-import React from "react";
 
 function LoginPage() {
   return (
-    <div>
-      LoginPage
+    <div className="h-screen hero">
       <SignIn />
     </div>
   );
 }
-
 export default LoginPage;
